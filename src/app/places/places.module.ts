f985bafe8a0b8113df98083dfd1d88fd1c306090
Filip Naumoski <filip.naumoski@gmail.com>
2019-07-08 15:10:23 +0200

    Added Tabs

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
+import { PlacesRoutingModule } from './places-routing.module';
 
 import { PlacesPage } from './places.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
-];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    PlacesRoutingModule
   ],
   declarations: [PlacesPage]
 })
