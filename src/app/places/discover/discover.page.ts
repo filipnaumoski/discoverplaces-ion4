85078313239937ecfb538a29f0f005f4fe68e181
Filip Naumoski <filip.naumoski@gmail.com>
2019-07-09 16:15:47 +0200

    Set up menu toggle

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
+
 import { Place } from '../places.model';
 
 @Component({
@@ -8,8 +9,10 @@ import { Place } from '../places.model';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
-  loadedPlaces: Place [];
-  constructor(private placesService: PlacesService) { }
+  loadedPlaces: Place[];
+  constructor(
+    private placesService: PlacesService
+  ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
