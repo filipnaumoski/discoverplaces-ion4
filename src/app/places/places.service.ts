8844ed72445df51f55a47b996a1f21f753f42dfb
Filip Naumoski <filip.naumoski@gmail.com>
2019-07-12 13:21:28 +0200

    Created booking form

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -12,21 +12,27 @@ export class PlacesService {
       'Manhetten Mansion',
       'In the heart of New York City.',
       'https://images.pexels.com/photos/466685/pexels-photo-466685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L\'Amour Toujours',
       'A romantic place in Paris!',
       'https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your avarege city trip',
       'https://images.pexels.com/photos/1141853/pexels-photo-1141853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
